87a8cb21b21b7fa3ef7c4dbe7234b01bc49e3c8a
Vibhor Mathur <matvibhor24@gmail.com>
2024-01-19 23:38:15 +0530

    Changed Favicon & logo

--- src/components/FunctionButtonBar.js
+++ src/components/FunctionButtonBar.js
@@ -2,21 +2,22 @@ import React from 'react'
 
 export default function FunctionButtonBar() {
   return (
-    <div className="btn-group function-button-bar" role="group" aria-label="Button group with nested dropdown">
-    <button type="button" class="btn btn-secondary">1</button>
-    <button type="button" class="btn btn-secondary">2</button>
+    <></>
+    // <div className="btn-group function-button-bar" role="group" aria-label="Button group with nested dropdown">
+    // <button type="button" class="btn btn-secondary">1</button>
+    // <button type="button" class="btn btn-secondary">2</button>
 
-    <div class="btn-group" role="group">
-    <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown
-    </button>
-    <div class="dropdown-menu" aria-labelled by="btnGroupDrop1">
-      <a class="dropdown-item" href="#">Dropdown link</a>
-      <a class="dropdown-item" href="#">Dropdown link</a>
-      <a class="dropdown-item" href="#">Dropdown link</a>
-    </div>
-    </div>
-    </div> 
+    // <div class="btn-group" role="group">
+    // <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    // Dropdown
+    // </button>
+    // <div class="dropdown-menu" aria-label by="btnGroupDrop1">
+    //   <a class="dropdown-item" href="#">Dropdown link</a>
+    //   <a class="dropdown-item" href="#">Dropdown link</a>
+    //   <a class="dropdown-item" href="#">Dropdown link</a>
+    // </div>
+    // </div>
+    // </div> 
     // <div>FunctionButtonBar</div>
   )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,8 +1,8 @@
 import React,{useState} from "react";
 // import up_animation from './up_animation.gif'
 import './TextForm.css'
-// import icon from './copy_icon.png';
-import FunctionButtonBar from "./FunctionButtonBar";
+import icon from './copy_icon.png';
+// import FunctionButtonBar from "./FunctionButtonBar";
 
 export default function TextForm(props){
     const[text,setText] = useState("");
@@ -14,55 +14,55 @@ export default function TextForm(props){
         setDisplayBoxVisible(false);
         // setText1("");
     }
-    // const onUpClick=()=>{
-    //     setDisplayBoxVisible(true);
-    //     setText1(text.toUpperCase());
-    // }
-    // const onLoClick=()=>{
-    //     setDisplayBoxVisible(true);
-    //     setText1(text.toLowerCase());
-    // }
-    // const convertToTitleCase = () => {
-    //     setDisplayBoxVisible(true);
-    //     const titleCaseText = text.replace(/\w\S*/g, (word) =>
-    //       word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
-    // );
-    //     setText1(titleCaseText);
-    //   };
-    // const onClearHandler=()=>{
-    //     setDisplayBoxVisible(false);
-    //     document.getElementById("exampleFormControlTextarea1").value="";
-    //     setText("");
-    //     setText1("");
-    // }
-    // const onCopyHandler=()=>{
-    //     // document.getElementById("display-box").select();
-    //     navigator.clipboard.writeText(displayText);
-    //     alert("copied to clipboard!");
-    // }
-    // const reverseText = () => {
-    //     setDisplayBoxVisible(true);
-    //     const reversedText = text.split('').reverse().join('');
-    //     setText1(reversedText);
-    // }
-    // const onRemoveSpaceHandler=()=>{
-    //     setDisplayBoxVisible(true);
-    //     let newText = text.split(" ");
-    //     setText1(newText.join(" "));
-    // }
-    // const removeSpecialCharacters = () => {
-    //     setDisplayBoxVisible(true);
-    //     const textWithoutSpecialChars = text.replace(/[^\w\s]/gi, '');
-    //     setText1(textWithoutSpecialChars);
-    //   }
+    const onUpClick=()=>{
+        setDisplayBoxVisible(true);
+        setText1(text.toUpperCase());
+    }
+    const onLoClick=()=>{
+        setDisplayBoxVisible(true);
+        setText1(text.toLowerCase());
+    }
+    const convertToTitleCase = () => {
+        setDisplayBoxVisible(true);
+        const titleCaseText = text.replace(/\w\S*/g, (word) =>
+          word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+    );
+        setText1(titleCaseText);
+      };
+    const onClearHandler=()=>{
+        setDisplayBoxVisible(false);
+        document.getElementById("exampleFormControlTextarea1").value="";
+        setText("");
+        setText1("");
+    }
+    const onCopyHandler=()=>{
+        // document.getElementById("display-box").select();
+        navigator.clipboard.writeText(displayText);
+        alert("copied to clipboard!");
+    }
+    const reverseText = () => {
+        setDisplayBoxVisible(true);
+        const reversedText = text.split('').reverse().join('');
+        setText1(reversedText);
+    }
+    const onRemoveSpaceHandler=()=>{
+        setDisplayBoxVisible(true);
+        let newText = text.split(" ");
+        setText1(newText.join(" "));
+    }
+    const removeSpecialCharacters = () => {
+        setDisplayBoxVisible(true);
+        const textWithoutSpecialChars = text.replace(/[^\w\s]/gi, '');
+        setText1(textWithoutSpecialChars);
+      }
     // let readTime = text.split(" ").length * 0.008;
     return (
         <>
         <div className="comp_container">
             <div className={`heading-tab text-${props.mode==='light'?'black':'white'}`}>
                 <h5>{props.heading}</h5>
-                <FunctionButtonBar/>
-            {/* <div className={`btn-group bg-${props.mode==='light'?'success-subtle':'success'} btn-outline-success shadow-lg rounded`} align="right" role="group" aria-label="Basic example" >
+                {/* <FunctionButtonBar/> */}
+            <div className={`btn-group bg-${props.mode==='light'?'success-subtle':'success'} btn-outline-success shadow-lg rounded`} align="right" role="group" aria-label="Basic example" >
                 <button type="button" className="btn " onClick={onUpClick} >To Uppercase</button>
                 <button type="button" className="btn " onClick={onLoClick}>To lowercase</button>
                 <button type="button" className="btn " onClick={convertToTitleCase}>To Titlecase</button>
@@ -70,15 +70,15 @@ export default function TextForm(props){
                 <button type="button" className="btn " onClick={onRemoveSpaceHandler}>Remove Extra spaces</button>
                 <button type="button" className="btn " onClick={removeSpecialCharacters}>Remove Special Characters</button>
                 <button type="button" className="btn " onClick={reverseText}>Reverse Text</button>
-            </div> */}
+            </div>
             </div>
 
             <div className={`mb-5 `}>
             <textarea className={`form-control bg-${props.mode==='light'?'white':'dark-subtle'} shadow p-3 mb-5 rounded`} value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" placeholder="Type here or Copy your text here..." rows="8"> </textarea>
             <div className={`display-box container shadow p-3 mb-5 rounded my-3 alert bg-${props.mode==='light'?'success-subtle':'success'} ${displayBoxVisible ? 'd-block' : 'd-none'} text-${props.mode==='light'?'black':'white'}`} role="alert" align="left">
-                {/* <button className="image-button" onClick={onCopyHandler} style={{border:"none",background:"transparent",cursor: "pointer"}}>
+                <button className="image-button" onClick={onCopyHandler} style={{border:"none",background:"transparent",cursor: "pointer"}}>
                     <img src={icon} alt="hii" />
-                </button> */}
+                </button>
                 {/* <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button> */}
                 <p>{displayText}</p>
             </div>
