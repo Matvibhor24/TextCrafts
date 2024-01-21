64fdaea9d5c43103bd48d7925a7c52fa1bff247a
Vibhor Mathur <matvibhor24@gmail.com>
2024-01-21 14:35:56 +0530

    About page edits

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './About.css'
+import bgLogo from './textcrafts.png'
+
 export default function About(props) { 
   const AboutBGStyle = {
     backgroundColor: props.mode==='light'?'#333':'#198754'
@@ -7,15 +9,20 @@ export default function About(props) {
   return (
     <>
     {/* <div className="header" > */}
-    <div className={`header shadow p-3 text-${props.mode==='light'?'white':'black'}`} style={AboutBGStyle}>
-      <h1 className="header">About Us</h1>
-      <img className="image" src="/path-to-your-image.jpg" alt="People gathered around"/>
-      <p className="content">
-        We’re a fully distributed team of 85 people living and working in 15 countries around
-        the world. And we’re working to build the best products to help our customers build
-        their brands and grow their businesses on social media.
-      </p>
+    <div className={`head shadow p-3 text-${props.mode==='light'?'white':'black'}`} style={AboutBGStyle}>
+      <h1 className="head my-5" align="center" ><b>About Us</b></h1>
+      <div class="image-container">
+        <img className="image" src={bgLogo} alt="People gathered around" />
+      </div>
     </div>
+    <div className="container-fluid">
+        <p className={`content text-${props.mode==='light'?'black':'white'}`}>
+          We’re a fully distributed team of 85 people living and working in 15 countries around
+          the world. And we’re working to build the best products to help our customers build
+          their brands and grow their businesses on social media.
+        </p>
+    </div>
+    {/* <img src={bgLogo} alt="Background" className="background-image" /> */}
     </>
     
   )
